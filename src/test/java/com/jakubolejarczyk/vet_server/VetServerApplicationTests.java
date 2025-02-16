48ecf8f4ff27014bb08297e3050bbc6f5162f550
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-16 15:40:13 +0100

    feat: Create an example of controller HelloController

---
+++ src/test/java/com/jakubolejarczyk/vet_server/VetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubolejarczyk.vet_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
