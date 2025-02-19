6f44214b3e4a9b90b71f82fac5b4bf7f7daa60b4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-20 00:29:28 +0100

    feat: List the tables without columns.

--- docker/init.sql
+++ docker/init.sql
@@ -1,8 +1,43 @@
-CREATE TABLE account (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255)
-);
+-- The script initializes the database structure for the vet application.
+-- It also populates the database with some default data.
 
-INSERT INTO account (first_name, last_name) VALUES ('Jan', 'Kowalski');
-INSERT INTO account (first_name, last_name) VALUES ('Paweł', 'Wolski');
+-- Create tables.
+CREATE TABLE Clinic ();
+
+CREATE TABLE Medication ();
+
+CREATE TABLE Clinic_Contact ();
+
+CREATE TABLE Contact ();
+
+CREATE TABLE Owner ();
+
+CREATE TABLE Account_Contact ();
+
+CREATE TABLE Account ();
+
+CREATE TABLE Clinic_Account ();
+
+CREATE TABLE OpeningHours ();
+
+CREATE TABLE Vet ();
+
+CREATE TABLE Client ();
+
+CREATE TABLE Client_Pet ();
+
+CREATE TABLE Pet ();
+
+CREATE TABLE Appointment ();
+
+CREATE TABLE Invoice ();
+
+CREATE TABLE Appointment_Service ();
+
+CREATE TABLE Service ();
+
+CREATE TABLE Service_Clinic ();
+
+CREATE TABLE MedicalRecord ();
+
+CREATE TABLE MedicalRecord_Medication ();
