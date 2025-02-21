9787436fe8fa3d45ac5f98f6cac7cbf35b6da30b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-21 16:18:09 +0100

    feat: Create a basic CRUD operation and save data to the db.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/ClinicController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/ClinicController.java
@@ -1,5 +1,6 @@
 package com.jakubolejarczyk.vet_server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/clinic")
 public class ClinicController {
+    private final ContactService contactService;
+
+    @Autowired
+    public ClinicController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
     @GetMapping("/get-clinics")
     public String getClinics() {
+        this.contactService.save();
         return "Hello Clinics!";
     }
 }
