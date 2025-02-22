29e7635da50b20a71d99cffcfca2deeefafac322
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-22 18:35:46 +0100

    feat: Change the Get to Post method.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/LoginController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/LoginController.java
@@ -1,16 +1,13 @@
 package com.jakubolejarczyk.vet_server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jakubolejarczyk.vet_server.dto.request.LoginRequestDto;
 
 @RestController
 @RequestMapping("/api/v1/login")
 public class LoginController {
-    @GetMapping
+    @PostMapping
     public String login(@RequestBody LoginRequestDto dto) {
         return "Email: " + dto.getEmail() + ", Password: " + dto.getPassword();
     }
