185f6a69299c781e1ea8f341b76338ae8680b426
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-23 18:26:55 +0100

    feat: Create basic client server commpunitaction.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/LoginController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/LoginController.java
@@ -1,14 +1,22 @@
 package com.jakubolejarczyk.vet_server.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.jakubolejarczyk.vet_server.dto.request.LoginRequestDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/login")
 public class LoginController {
     @PostMapping
-    public String login(@RequestBody LoginRequestDto dto) {
-        return "Email: " + dto.getEmail() + ", Password: " + dto.getPassword();
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequestDto dto) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("email", dto.getEmail());
+        response.put("password", dto.getPassword());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
