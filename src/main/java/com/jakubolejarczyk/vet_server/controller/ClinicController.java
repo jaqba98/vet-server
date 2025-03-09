0732c2fc4ba1bbab5009894d867c80a96700d186
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-09 04:13:47 +0100

    fix: Move hangle validation to the specific controller with repsonse model.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/ClinicController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/ClinicController.java
@@ -6,10 +6,10 @@ import com.jakubolejarczyk.vet_server.service.database.ClinicService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,6 +22,21 @@ public class ClinicController {
         String name = requestDto.getName();
         clinicService.create(name);
         ClinicCreateResponseDto responseDto = ClinicCreateResponseDto.builder().success(true).build();
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ClinicCreateResponseDto> handleValidation(MethodArgumentNotValidException ex) {
+        ArrayList<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String message = error.getDefaultMessage();
+            errors.add(message);
+        });
+        ClinicCreateResponseDto responseDto = ClinicCreateResponseDto
+                .builder()
+                .success(false)
+                .errors(errors)
+                .build();
         return ResponseEntity.ok().body(responseDto);
     }
 }
--- src/main/java/com/jakubolejarczyk/vet_server/dto/response/clinic/ClinicCreateResponseDto.java
+++ src/main/java/com/jakubolejarczyk/vet_server/dto/response/clinic/ClinicCreateResponseDto.java
@@ -5,10 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 @SuperBuilder
 @NoArgsConstructor
 public class ClinicCreateResponseDto {
     private Boolean success;
+
+    private ArrayList<String> errors;
 }
