1c61c14804617383cd04c7e1e7a6de9d25e6877d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-17 06:05:42 +0100

    fix: Return response after registration/

--- src/main/java/com/jakubolejarczyk/vet_server/dto/response/ResponseDto.java
+++ src/main/java/com/jakubolejarczyk/vet_server/dto/response/ResponseDto.java
@@ -1,9 +1,13 @@
 package com.jakubolejarczyk.vet_server.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class ResponseDto<TData> {
     private Boolean success;
