78c19b661abfcc8fe95f617fcb1cc7573ffe7bc2
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-19 21:30:50 +0100

    refactor: Logout endpoint.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/common/LogoutController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/common/LogoutController.java
@@ -1,20 +1,36 @@
 package com.jakubolejarczyk.vet_server.controller.common;
 
+import com.jakubolejarczyk.vet_server.dto.request.controller.LogoutRequestDto;
+import com.jakubolejarczyk.vet_server.dto.response.ResponseDto;
+import com.jakubolejarczyk.vet_server.service.security.HandleValidationService;
+import com.jakubolejarczyk.vet_server.service.step.GetAccountByTokenStep;
+import com.jakubolejarczyk.vet_server.service.step.SuccessResponseStep;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@AllArgsConstructor
 public class LogoutController {
-//    @PostMapping("logout")
-//    public ResponseEntity<LogoutResponseDto> logout(@Valid @RequestBody LogoutRequestDto requestDto) {
-//        LogoutResponseDto responseDto = new LogoutResponseDto(true, new ArrayList<>());
-//        return ResponseEntity.ok().body(responseDto);
-//    }
-//
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<LogoutResponseDto> handleValidation(MethodArgumentNotValidException ex) {
-//        ArrayList<String> errors = new ArrayList<>();
-//        ex.getBindingResult().getAllErrors().forEach((error) -> {
-//            String message = error.getDefaultMessage();
-//            errors.add(message);
-//        });
-//        LogoutResponseDto responseDto = new LogoutResponseDto(false, errors);
-//        return ResponseEntity.ok().body(responseDto);
-//    }
+    private final GetAccountByTokenStep<String> getAccountByTokenStep;
+    private final SuccessResponseStep<String> successResponseStep;
+    private final HandleValidationService handleValidationService;
+
+    @PostMapping("logout")
+    public ResponseEntity<ResponseDto<String>> logout(@Valid @RequestBody LogoutRequestDto requestDto) {
+        val account = getAccountByTokenStep.getAccount(requestDto.getToken());
+        if (account.isEmpty()) {
+            return getAccountByTokenStep.buildErrorResponse("");
+        }
+        return successResponseStep.getSuccessResponse("Logged out", "");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto<String>> handleValidation(MethodArgumentNotValidException ex) {
+        return handleValidationService.handle(ex);
+    }
 }
