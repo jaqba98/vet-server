21af1651ae7528cdb117e240d6a2144df903fdd7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-28 05:05:07 +0100

    refactor: CreateClinicStep step

--- src/main/java/com/jakubolejarczyk/vet_server/service/input/create/CreateClinicInput.java
+++ src/main/java/com/jakubolejarczyk/vet_server/service/input/create/CreateClinicInput.java
@@ -1,16 +1,16 @@
 package com.jakubolejarczyk.vet_server.service.input.create;
 
 public record CreateClinicInput(
-        String name,
-        String street,
-        String buildingNumber,
-        String apartmentNumber,
-        String postalCode,
-        String city,
-        String province,
-        String country,
-        String email,
-        String phoneNumber,
-        Long openingHoursId
+    String name,
+    String street,
+    String buildingNumber,
+    String apartmentNumber,
+    String postalCode,
+    String city,
+    String province,
+    String country,
+    String email,
+    String phoneNumber,
+    Long openingHoursId
 ) {
 }
--- src/main/java/com/jakubolejarczyk/vet_server/service/step/create/CreateClinicStep.java
+++ src/main/java/com/jakubolejarczyk/vet_server/service/step/create/CreateClinicStep.java
@@ -6,25 +6,24 @@ import com.jakubolejarczyk.vet_server.service.crud.independent.OpeningHoursServi
 import com.jakubolejarczyk.vet_server.service.input.create.CreateClinicInput;
 import com.jakubolejarczyk.vet_server.service.model.StepModel;
 import com.jakubolejarczyk.vet_server.service.model.StepOutput;
+import com.jakubolejarczyk.vet_server.service.output.create.CreateClinicOutput;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class CreateClinicStep implements StepModel<CreateClinicInput, Clinic> {
+public class CreateClinicStep implements StepModel<CreateClinicInput, CreateClinicOutput> {
     private final OpeningHoursService openingHoursService;
     private final ClinicService clinicService;
 
     @Override
-    public StepOutput<Clinic> runStep(CreateClinicInput input) {
+    public StepOutput<CreateClinicOutput> runStep(CreateClinicInput input) {
         try {
             val openingHours = openingHoursService.findById(input.openingHoursId());
             if (openingHours.isEmpty()) {
-                return StepOutput.<Clinic>builder()
-                        .success(false)
-                        .message("Failed to create clinic. Opening hours do not exist!")
-                        .build();
+                val output = new CreateClinicOutput(Clinic.builder().build());
+                return new StepOutput<>(false, "Failed to create clinic.", output);
             }
             val clinic = Clinic.builder()
                     .name(input.name())
@@ -41,10 +40,8 @@ public class CreateClinicStep implements StepModel<CreateClinicInput, Clinic> {
                     .openingHoursId(input.openingHoursId())
                     .build();
             val newClinic = clinicService.create(clinic);
-            return StepOutput.<Clinic>builder()
-                    .success(true)
-                    .data(newClinic)
-                    .build();
+            val output = new CreateClinicOutput(newClinic);
+            return new StepOutput<>(true, "The clinic has been established", output);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
