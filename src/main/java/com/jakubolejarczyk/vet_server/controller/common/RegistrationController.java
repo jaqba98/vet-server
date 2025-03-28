90a2496512ca5a1e4eff7f68ab978dd01ff03134
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-28 14:34:58 +0100

    refactor: refactor the REgistrationController

--- src/main/java/com/jakubolejarczyk/vet_server/controller/common/RegistrationController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/common/RegistrationController.java
@@ -1,48 +1,48 @@
-//package com.jakubolejarczyk.vet_server.controller.common;
-//
-//import com.jakubolejarczyk.vet_server.controller.base.BaseController;
-//import com.jakubolejarczyk.vet_server.dto.request.common.RegistrationRequest;
-//import com.jakubolejarczyk.vet_server.dto.response.Response;
-//import com.jakubolejarczyk.vet_server.service.input.create.CreateAccountInput;
-//import com.jakubolejarczyk.vet_server.service.response.ResponseService;
-//import com.jakubolejarczyk.vet_server.service.security.HandleValidationService;
-//import com.jakubolejarczyk.vet_server.service.step.create.CreateAccountStep;
-//import jakarta.validation.Valid;
-//import jakarta.validation.constraints.Null;
-//import lombok.val;
-//import org.springframework.beans.factory.ObjectFactory;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/v1")
-//public class RegistrationController extends BaseController<RegistrationRequest, Null, Null> {
-//    private final CreateAccountStep createAccountStep;
-//
-//    protected RegistrationController(
-//            ObjectFactory<HandleValidationService> handleValidationService,
-//            ObjectFactory<ResponseService<Null, Null>> responseService,
-//            CreateAccountStep createAccountStep
-//    ) {
-//        super(handleValidationService, responseService);
-//        this.createAccountStep = createAccountStep;
-//    }
-//
-//    @Override
-//    @PostMapping("registration")
-//    public ResponseEntity<Response<Null, Null>>
-//    runController(@Valid @RequestBody RegistrationRequest requestDto) {
-//        responseService.getObject().cleanUp();
-//        val createAccountResponse = createAccountStep.runStep(new CreateAccountInput(
-//                requestDto.getEmail(),
-//                requestDto.getPassword(),
-//                requestDto.getFirstName(),
-//                requestDto.getLastName()
-//        ));
-//        val success = createAccountResponse.getSuccess();
-//        val message = createAccountResponse.getMessage();
-//        responseService.getObject().addMessage(message);
-//        return responseService.getObject().getResponse(success, null, null);
-//    }
-//}
-//
+package com.jakubolejarczyk.vet_server.controller.common;
+
+import com.jakubolejarczyk.vet_server.controller.base.BaseController;
+import com.jakubolejarczyk.vet_server.dto.request.common.LoginRequest;
+import com.jakubolejarczyk.vet_server.dto.request.common.RegistrationRequest;
+import com.jakubolejarczyk.vet_server.dto.response.Response;
+import com.jakubolejarczyk.vet_server.service.security.HandleValidationService;
+import com.jakubolejarczyk.vet_server.service.step.create.CreateAccountStep;
+import com.jakubolejarczyk.vet_server.service.step.get.GetTokenByLoginDetailsStep;
+import com.jakubolejarczyk.vet_server.service.store.StepStore;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RegistrationController extends BaseController {
+    public RegistrationController(
+            ObjectFactory<StepStore> stepStore,
+            ObjectFactory<HandleValidationService> handleValidationService,
+            CreateAccountStep createAccountStep
+    ) {
+        super(
+                stepStore,
+                handleValidationService,
+                new String[]{},
+                new String[]{},
+                new ArrayList<>()
+        );
+        steps.addLast(createAccountStep);
+    }
+
+    @PostMapping("registration")
+    public ResponseEntity<Response<?, ?>> runController(@Valid @RequestBody RegistrationRequest requestDto) {
+        initController();
+        stepStore.getObject().setItem("email", requestDto.getEmail());
+        stepStore.getObject().setItem("password", requestDto.getPassword());
+        stepStore.getObject().setItem("firstName", requestDto.getFirstName());
+        stepStore.getObject().setItem("lastName", requestDto.getLastName());
+        return runController(steps);
+    }
+}
--- src/main/java/com/jakubolejarczyk/vet_server/service/step/create/CreateAccountStep.java
+++ src/main/java/com/jakubolejarczyk/vet_server/service/step/create/CreateAccountStep.java
@@ -1,49 +1,40 @@
-//package com.jakubolejarczyk.vet_server.service.step.create;
-//
-//import com.jakubolejarczyk.vet_server.model.independent.Account;
-//import com.jakubolejarczyk.vet_server.service.crud.independent.AccountService;
-//import com.jakubolejarczyk.vet_server.service.input.create.CreateAccountInput;
-//import com.jakubolejarczyk.vet_server.service.model.StepModel;
-//import com.jakubolejarczyk.vet_server.service.model.StepOutput;
-//import com.jakubolejarczyk.vet_server.service.security.PasswordService;
-//import lombok.AllArgsConstructor;
-//import lombok.val;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class CreateAccountStep implements StepModel<CreateAccountInput, Account> {
-//    private final AccountService accountService;
-//    private final PasswordService passwordService;
-//
-//    @Override
-//    public StepOutput<Account> runStep(CreateAccountInput input) {
-//        try {
-//            val email = input.email();
-//            val accountByEmail = accountService.findByEmail(email);
-//            if (accountByEmail.isPresent()) {
-//                return StepOutput.<Account>builder()
-//                        .success(false)
-//                        .build();
-//            }
-//            val password = input.password();
-//            val firstName = input.firstName();
-//            val lastName = input.lastName();
-//            val hashPassword = passwordService.encode(password);
-//            val account = Account.builder()
-//                    .email(input.email())
-//                    .password(hashPassword)
-//                    .firstName(firstName)
-//                    .lastName(lastName)
-//                    .isArchived(false)
-//                    .build();
-//            val newAccount = accountService.create(account);
-//            return StepOutput.<Account>builder()
-//                    .success(true)
-//                    .output(newAccount)
-//                    .build();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
+package com.jakubolejarczyk.vet_server.service.step.create;
+
+import com.jakubolejarczyk.vet_server.model.independent.Account;
+import com.jakubolejarczyk.vet_server.service.crud.independent.AccountService;
+import com.jakubolejarczyk.vet_server.service.model.StepModel;
+import com.jakubolejarczyk.vet_server.service.security.PasswordService;
+import com.jakubolejarczyk.vet_server.service.store.StepStore;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CreateAccountStep implements StepModel {
+    private final AccountService accountService;
+    private final PasswordService passwordService;
+
+    @Override
+    public void runStep(StepStore stepStore) {
+        val email = (String) stepStore.getItem("email");
+        val accountByEmail = accountService.findByEmail(email);
+        if (accountByEmail.isPresent()) {
+            stepStore.setSuccess(false);
+            return;
+        }
+        val password = (String) stepStore.getItem("password");
+        val firstName = (String) stepStore.getItem("firstName");
+        val lastName = (String) stepStore.getItem("lastName");
+        val hashPassword = passwordService.encode(password);
+        val account = Account.builder()
+            .email(email)
+            .password(hashPassword)
+            .firstName(firstName)
+            .lastName(lastName)
+            .isArchived(false)
+            .build();
+        val newAccount = accountService.create(account);
+        stepStore.setSuccess(true);
+    }
+}
