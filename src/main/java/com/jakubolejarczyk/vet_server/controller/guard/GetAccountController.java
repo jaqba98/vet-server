3d0e61590b310421035b17e44b8512313d122adc
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-03-30 19:42:03 +0200

    refactor: GetAccountController controller.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/guard/GetAccountController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/guard/GetAccountController.java
@@ -1,43 +1,45 @@
-//package com.jakubolejarczyk.vet_server.controller.guard;
-//
-//import com.jakubolejarczyk.vet_server.dto.request.base.TokenRequest;
-//import com.jakubolejarczyk.vet_server.dto.response.Response;
-//import com.jakubolejarczyk.vet_server.service.security.HandleValidationService;
-//import com.jakubolejarczyk.vet_server.service.step.get.GetAccountByTokenStep;
-//import com.jakubolejarczyk.vet_server.step.base.BaseController;
-//import com.jakubolejarczyk.vet_server.store.StepStore;
-//import jakarta.validation.Valid;
-//import org.springframework.beans.factory.ObjectFactory;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.ArrayList;
-//
-//@RestController
-//@RequestMapping("/api/v1")
-//public class GetAccountController extends BaseController {
-//    public GetAccountController(
-//                ObjectFactory<StepStore> stepStore,
-//                ObjectFactory<HandleValidationService> handleValidationService,
-//                GetAccountByTokenStep getAccountByTokenStep
-//    ) {
-//        super(
-//                stepStore,
-//                handleValidationService,
-//                new String[]{"email", "firstName", "lastName", "role", "pictureUrl"},
-//                new String[]{},
-//                new ArrayList<>()
-//        );
-//        steps.addLast(getAccountByTokenStep);
-//    }
-//
-//    @PostMapping("get-account")
-//    public ResponseEntity<Response<?, ?>> runController(@Valid @RequestBody TokenRequest requestDto) {
-//        initController();
-//        stepStore.getObject().setItem("token", requestDto.getToken());
-//        return runController(steps);
-//    }
-//}
+package com.jakubolejarczyk.vet_server.controller.guard;
+
+import com.jakubolejarczyk.vet_server.dto.request.base.TokenRequest;
+import com.jakubolejarczyk.vet_server.dto.response.Response;
+import com.jakubolejarczyk.vet_server.security.HandleValidationService;
+import com.jakubolejarczyk.vet_server.step.base.BaseController;
+import com.jakubolejarczyk.vet_server.step.get.GetAccountByTokenStep;
+import com.jakubolejarczyk.vet_server.step.model.StepModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import jakarta.validation.Valid;
+import lombok.val;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GetAccountController extends BaseController {
+    private final GetAccountByTokenStep getAccountByTokenStep;
+
+    public GetAccountController(
+            ObjectFactory<StepStore> stepStoreObjectFactory,
+            ObjectFactory<HandleValidationService> handleValidationServiceObjectFactory,
+            GetAccountByTokenStep getAccountByTokenStep
+    ) {
+        super(stepStoreObjectFactory, handleValidationServiceObjectFactory);
+        this.getAccountByTokenStep = getAccountByTokenStep;
+    }
+
+    @PostMapping("get-account")
+    public ResponseEntity<Response<?, ?>> validToken(@Valid @RequestBody TokenRequest request) {
+        val steps = new ArrayList<StepModel>();
+        steps.addLast(getAccountByTokenStep);
+        String[] dataKeys = {"email", "firstName", "lastName", "role", "pictureUrl"};
+        String[] metadataKeys = {};
+        initController(dataKeys, metadataKeys);
+        getStepStore().setItem("token", request.getToken());
+        return runController(steps);
+    }
+}
