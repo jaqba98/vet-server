b25ed0d0f48764285a87f5ed98461a41eaa02951
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 01:08:27 +0200

    refactor: Pet delete controller.

---
+++ src/main/java/com/jakubolejarczyk/vet_server/step/delete/DeletePetStep.java
@@ -0,0 +1,21 @@
+package com.jakubolejarczyk.vet_server.step.delete;
+
+import com.jakubolejarczyk.vet_server.service.dependent.PetService;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DeletePetStep<TData, TMetadata> implements StepRunnerModel<TData, TMetadata> {
+    private final PetService petService;
+
+    @Override
+    public void runStep(StepStore<TData, TMetadata> stepStore) {
+        if (stepStore.hasNotItem("petIds")) throw new Error("The petIds is required!");
+        val petIds = stepStore.getItemAsArray("petIds", Long.class);
+        petService.deleteAllById(petIds);
+    }
+}
---
+++ src/main/java/com/jakubolejarczyk/vet_server/step/response/dependent/pet/PetDeleteResponseStep.java
@@ -0,0 +1,19 @@
+package com.jakubolejarczyk.vet_server.step.response.dependent.pet;
+
+import com.jakubolejarczyk.vet_server.dto.data.dependent.PetData;
+import com.jakubolejarczyk.vet_server.dto.metadata.dependent.PetMetadata;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PetDeleteResponseStep implements StepRunnerModel<PetData, PetMetadata> {
+    @Override
+    public void runStep(StepStore<PetData, PetMetadata> stepStore) {
+        if (stepStore.getSuccess()) {
+            stepStore.addMessage("The pet has been deleted successfully!");
+        }
+    }
+}
