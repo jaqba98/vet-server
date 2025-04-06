4cfea1e66f6ed58e5c7c430fb8303993a16d11fc
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 13:30:15 +0200

    refactor: Logout controller

--- src/main/java/com/jakubolejarczyk/vet_server/step/response/logic/LogoutResponseStep.java
+++ src/main/java/com/jakubolejarczyk/vet_server/step/response/logic/LogoutResponseStep.java
@@ -1,17 +1,17 @@
-//package com.jakubolejarczyk.vet_server.step.response.logic;
-//
-//import com.jakubolejarczyk.vet_server.dto.data.logic.LogoutData;
-//import com.jakubolejarczyk.vet_server.dto.metadata.logic.LogoutMetadata;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
-//import com.jakubolejarczyk.vet_server.store.StepStore;
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class LogoutResponseStep implements StepRunnerModel<LogoutData, LogoutMetadata> {
-//    @Override
-//    public void runStep(StepStore<LogoutData, LogoutMetadata> stepStore) {
-//        stepStore.addMessage("You have logged out successfully!");
-//    }
-//}
+package com.jakubolejarczyk.vet_server.step.response.logic;
+
+import com.jakubolejarczyk.vet_server.dto.data.logic.LogoutData;
+import com.jakubolejarczyk.vet_server.dto.metadata.logic.LogoutMetadata;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LogoutResponseStep implements StepRunnerModel<LogoutData, LogoutMetadata> {
+    @Override
+    public void runStep(StepStore<LogoutData, LogoutMetadata> stepStore) {
+        stepStore.addMessage("You have logged out successfully!");
+    }
+}
