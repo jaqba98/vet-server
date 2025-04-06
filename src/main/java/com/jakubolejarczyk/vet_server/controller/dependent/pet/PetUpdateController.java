652425a443d29bde4e8d22935e35675affcef62f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 02:02:36 +0200

    refactor: Pet update controller.

--- src/main/java/com/jakubolejarczyk/vet_server/controller/dependent/pet/PetUpdateController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/dependent/pet/PetUpdateController.java
@@ -1,72 +1,85 @@
-//package com.jakubolejarczyk.vet_server.controller.old.pet;
-//
-//import com.jakubolejarczyk.vet_server.dto.request.dependent.PetRequest;
-//import com.jakubolejarczyk.vet_server.dto.response.Response;
-//import com.jakubolejarczyk.vet_server.model.dependent.Pet;
-//import com.jakubolejarczyk.vet_server.security.HandleValidationService;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerController;
-//import com.jakubolejarczyk.vet_server.step.check.CheckClientExistStepRunner;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
-//import com.jakubolejarczyk.vet_server.step.success.SuccessUpdatePetStepRunner;
-//import com.jakubolejarczyk.vet_server.step.update.UpdatePetStepRunner;
-//import com.jakubolejarczyk.vet_server.store.StepStore;
-//import jakarta.validation.Valid;
-//import lombok.val;
-//import org.springframework.beans.factory.ObjectFactory;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.ArrayList;
-//
-//@RestController
-//@RequestMapping("/api/v1")
-//public class PetUpdateController extends StepRunnerController {
-//    private final CheckClientExistStepRunner checkClientExistStep;
-//    private final UpdatePetStepRunner updatePetStep;
-//    private final SuccessUpdatePetStepRunner successUpdatePetStep;
-//
-//    public PetUpdateController(
-//            ObjectFactory<StepStore> stepStoreObjectFactory,
-//            ObjectFactory<HandleValidationService> handleValidationServiceObjectFactory,
-//            CheckClientExistStepRunner checkClientExistStep,
-//            UpdatePetStepRunner updatePetStep,
-//            SuccessUpdatePetStepRunner successUpdatePetStep
-//    ) {
-//        super(stepStoreObjectFactory, handleValidationServiceObjectFactory);
-//        this.checkClientExistStep = checkClientExistStep;
-//        this.updatePetStep = updatePetStep;
-//        this.successUpdatePetStep = successUpdatePetStep;
-//    }
-//
-//    @PostMapping("pet-update")
-//    public ResponseEntity<Response<?, ?>> petUpdate(@Valid @RequestBody PetRequest request) {
-//        val steps = new ArrayList<StepRunnerModel>();
-//        steps.addLast(checkClientExistStep);
-//        steps.addLast(updatePetStep);
-//        steps.addLast(successUpdatePetStep);
-//        String[] dataKeys = {"pet"};
-//        String[] metadataKeys = {};
-//        initController(dataKeys, metadataKeys);
-//        getStepStore().setItem("token", request.getToken());
-//        val requestPet = Pet.builder()
-//                .id(request.getId())
-//                .fullName(request.getFullName())
-//                .species(request.getSpecies())
-//                .breed(request.getBreed())
-//                .dateOfBirth(request.getDateOfBirth())
-//                .weightKg(request.getWeightKg())
-//                .color(request.getColor())
-//                .sterilized(request.getSterilized())
-//                .pictureUrl(request.getPictureUrl())
-//                .microchipNumber(request.getMicrochipNumber())
-//                .medicalNotes(request.getMedicalNotes())
-//                .clientId(request.getClientId())
-//                .build();
-//        getStepStore().setItem("requestPet", requestPet);
-//        getStepStore().setItem("clientId", requestPet.getClientId());
-//        return runController(steps);
-//    }
-//}
+package com.jakubolejarczyk.vet_server.controller.dependent.pet;
+
+import com.jakubolejarczyk.vet_server.dto.data.dependent.PetData;
+import com.jakubolejarczyk.vet_server.dto.metadata.dependent.PetMetadata;
+import com.jakubolejarczyk.vet_server.dto.request.dependent.PetRequest;
+import com.jakubolejarczyk.vet_server.dto.response.Response;
+import com.jakubolejarczyk.vet_server.model.dependent.Pet;
+import com.jakubolejarczyk.vet_server.security.HandleValidationService;
+import com.jakubolejarczyk.vet_server.step.check.CheckAccountPermissionToClientStep;
+import com.jakubolejarczyk.vet_server.step.create.CreatePetStep;
+import com.jakubolejarczyk.vet_server.step.get.GetAccountByTokenStep;
+import com.jakubolejarczyk.vet_server.step.get.GetClientByIdStep;
+import com.jakubolejarczyk.vet_server.step.response.dependent.pet.PetCreateResponseStep;
+import com.jakubolejarczyk.vet_server.step.response.dependent.pet.PetUpdateResponseStep;
+import com.jakubolejarczyk.vet_server.step.update.UpdatePetStep;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerController;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import jakarta.validation.Valid;
+import lombok.val;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PetUpdateController extends StepRunnerController<PetData, PetMetadata> {
+    private final GetAccountByTokenStep<PetData, PetMetadata> getAccountByTokenStep;
+    private final GetClientByIdStep<PetData, PetMetadata> getClientByIdStep;
+    private final CheckAccountPermissionToClientStep<PetData, PetMetadata> checkAccountPermissionToClientStep;
+    private final UpdatePetStep<PetData, PetMetadata> updatePetStep;
+    private final PetUpdateResponseStep petUpdateResponseStep;
+
+    public PetUpdateController(
+        ObjectFactory<StepStore<PetData, PetMetadata>> stepStoreObjectFactory,
+        ObjectFactory<HandleValidationService> handleValidationServiceObjectFactory,
+        GetAccountByTokenStep<PetData, PetMetadata> getAccountByTokenStep,
+        GetClientByIdStep<PetData, PetMetadata> getClientByIdStep,
+        CheckAccountPermissionToClientStep<PetData, PetMetadata> checkAccountPermissionToClientStep,
+        UpdatePetStep<PetData, PetMetadata> updatePetStep,
+        PetUpdateResponseStep petUpdateResponseStep
+    ) {
+        super(stepStoreObjectFactory, handleValidationServiceObjectFactory);
+        this.getAccountByTokenStep = getAccountByTokenStep;
+        this.getClientByIdStep = getClientByIdStep;
+        this.checkAccountPermissionToClientStep = checkAccountPermissionToClientStep;
+        this.updatePetStep = updatePetStep;
+        this.petUpdateResponseStep = petUpdateResponseStep;
+    }
+
+    @PostMapping("pet-update")
+    public ResponseEntity<Response<PetData, PetMetadata>> petUpdate(
+        @Valid @RequestBody PetRequest request
+    ) {
+        val steps = new ArrayList<StepRunnerModel<PetData, PetMetadata>>();
+        steps.addLast(getAccountByTokenStep);
+        steps.addLast(getClientByIdStep);
+        steps.addLast(checkAccountPermissionToClientStep);
+        steps.addLast(updatePetStep);
+        initController();
+        getStepStore().setItem("token", request.getToken());
+        val requestPet = Pet.builder()
+            .id(request.getId())
+            .fullName(request.getFullName())
+            .species(request.getSpecies())
+            .breed(request.getBreed())
+            .dateOfBirth(request.getDateOfBirth())
+            .weightKg(request.getWeightKg())
+            .color(request.getColor())
+            .sterilized(request.getSterilized())
+            .pictureUrl(request.getPictureUrl())
+            .microchipNumber(request.getMicrochipNumber())
+            .medicalNotes(request.getMedicalNotes())
+            .clientId(request.getClientId())
+            .build();
+        getStepStore().setItem("requestPet", requestPet);
+        getStepStore().setItem("clientId", requestPet.getClientId());
+        return runController(steps, petUpdateResponseStep);
+    }
+}
