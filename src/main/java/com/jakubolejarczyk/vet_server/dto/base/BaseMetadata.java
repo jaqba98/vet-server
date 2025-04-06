562ba75dc7fc690463300efa73f453a7cc57fccf
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 02:10:41 +0200

    fix: Set default value map.

--- src/main/java/com/jakubolejarczyk/vet_server/dto/base/BaseMetadata.java
+++ src/main/java/com/jakubolejarczyk/vet_server/dto/base/BaseMetadata.java
@@ -2,11 +2,12 @@ package com.jakubolejarczyk.vet_server.dto.base;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class BaseMetadata {
-    private Map<Long, String> values;
+    private Map<Long, String> values = new HashMap<>();
 
     public void addValue(Long key, String value) {
         values.put(key, value);
