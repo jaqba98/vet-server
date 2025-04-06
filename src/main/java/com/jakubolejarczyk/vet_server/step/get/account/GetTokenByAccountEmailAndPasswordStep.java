7b2e99a1438e2d6e3310981b9ca71c19b498b8f6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 10:53:45 +0200

    refactor: LoginController

---
+++ src/main/java/com/jakubolejarczyk/vet_server/step/get/account/GetTokenByAccountEmailAndPasswordStep.java
@@ -0,0 +1,37 @@
+package com.jakubolejarczyk.vet_server.step.get.account;
+
+import com.jakubolejarczyk.vet_server.security.PasswordService;
+import com.jakubolejarczyk.vet_server.security.TokenService;
+import com.jakubolejarczyk.vet_server.service.independent.AccountService;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GetTokenByAccountEmailAndPasswordStep<TData, TMetadata> implements StepRunnerModel<TData, TMetadata> {
+    private final AccountService accountService;
+    private final PasswordService passwordService;
+    private final TokenService tokenService;
+
+    @Override
+    public void runStep(StepStore<TData, TMetadata> stepStore) {
+        if (stepStore.hasNotItem("email")) throw new Error("The email is required!");
+        if (stepStore.hasNotItem("password")) throw new Error("The password is required!");
+        val email = stepStore.getItem("email", String.class);
+        val password = stepStore.getItem("password", String.class);
+        val account = accountService.findByEmail(email);
+        if (account.isPresent()) {
+            val encodedPassword = account.get().getPassword();
+            if (passwordService.match(password, encodedPassword)) {
+                val token = tokenService.generate(email);
+                stepStore.setItem("token", token);
+                return;
+            }
+        }
+        stepStore.setSuccess(false);
+        stepStore.addMessage("Incorrect email or password");
+    }
+}
--- src/main/java/com/jakubolejarczyk/vet_server/step/response/logic/LoginResponseStep.java
+++ src/main/java/com/jakubolejarczyk/vet_server/step/response/logic/LoginResponseStep.java
@@ -1,28 +1,26 @@
-//package com.jakubolejarczyk.vet_server.step.response.logic;
-//
-//import com.jakubolejarczyk.vet_server.dto.data.logic.LoginData;
-//import com.jakubolejarczyk.vet_server.dto.metadata.logic.LoginMetadata;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
-//import com.jakubolejarczyk.vet_server.store.StepStore;
-//import lombok.AllArgsConstructor;
-//import lombok.val;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class LoginResponseStep implements StepRunnerModel<LoginData, LoginMetadata> {
-//    @Override
-//    public void runStep(StepStore<LoginData, LoginMetadata> stepStore) {
-//        if (stepStore.getSuccess()) {
-//            if (stepStore.hasNotItem("token")) throw new Error("The token is required!");
-//            val token = stepStore.getItem("token", String.class);
-//            val data = new LoginData(token);
-//            val metadata = new LoginMetadata();
-//            stepStore.addMessage("You have logged in successfully!");
-//            stepStore.setData(data);
-//            stepStore.setMetadata(metadata);
-//            return;
-//        }
-//        stepStore.addMessage("Incorrect login or password!");
-//    }
-//}
+package com.jakubolejarczyk.vet_server.step.response.logic;
+
+import com.jakubolejarczyk.vet_server.dto.data.logic.LoginData;
+import com.jakubolejarczyk.vet_server.dto.metadata.logic.LoginMetadata;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LoginResponseStep implements StepRunnerModel<LoginData, LoginMetadata> {
+    @Override
+    public void runStep(StepStore<LoginData, LoginMetadata> stepStore) {
+        if (stepStore.getSuccess()) {
+            if (stepStore.hasNotItem("token")) throw new Error("The token is required!");
+            val token = stepStore.getItem("token", String.class);
+            val data = new LoginData(token);
+            val metadata = new LoginMetadata();
+            stepStore.addMessage("You have logged in successfully!");
+            stepStore.setData(data);
+            stepStore.setMetadata(metadata);
+        }
+    }
+}
