96d60b16d4da9913eb1f76a6eae351e99b776365
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 10:07:58 +0200

    refactor: ValidTokenController

--- src/main/java/com/jakubolejarczyk/vet_server/dto/request/base/TokenRequest.java
+++ src/main/java/com/jakubolejarczyk/vet_server/dto/request/base/TokenRequest.java
@@ -1,12 +1,10 @@
 package com.jakubolejarczyk.vet_server.dto.request.base;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TokenRequest {
-    @NotNull(message = "Token is required!")
     private String token;
 }
