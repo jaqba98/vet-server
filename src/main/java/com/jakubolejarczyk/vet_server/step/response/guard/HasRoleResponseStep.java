453677ab882a775461b4657f61eb4a859317ea3d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 11:08:42 +0200

    refactor: HasRoleController

--- src/main/java/com/jakubolejarczyk/vet_server/step/response/guard/HasRoleResponseStep.java
+++ src/main/java/com/jakubolejarczyk/vet_server/step/response/guard/HasRoleResponseStep.java
@@ -1,24 +1,24 @@
-//package com.jakubolejarczyk.vet_server.step.response.guard;
-//
-//import com.jakubolejarczyk.vet_server.dto.data.guard.HasRoleData;
-//import com.jakubolejarczyk.vet_server.dto.metadata.guard.HasRoleMetadata;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
-//import com.jakubolejarczyk.vet_server.store.StepStore;
-//import lombok.AllArgsConstructor;
-//import lombok.val;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class HasRoleResponseStep implements StepRunnerModel<HasRoleData, HasRoleMetadata> {
-//    @Override
-//    public void runStep(StepStore<HasRoleData, HasRoleMetadata> stepStore) {
-//        if (stepStore.getSuccess()) {
-//            val data = new HasRoleData();
-//            val metadata = new HasRoleMetadata();
-//            stepStore.addMessage("The account has a role");
-//            stepStore.setData(data);
-//            stepStore.setMetadata(metadata);
-//        }
-//    }
-//}
+package com.jakubolejarczyk.vet_server.step.response.guard;
+
+import com.jakubolejarczyk.vet_server.dto.data.guard.HasRoleData;
+import com.jakubolejarczyk.vet_server.dto.metadata.guard.HasRoleMetadata;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class HasRoleResponseStep implements StepRunnerModel<HasRoleData, HasRoleMetadata> {
+    @Override
+    public void runStep(StepStore<HasRoleData, HasRoleMetadata> stepStore) {
+        if (stepStore.getSuccess()) {
+            val data = new HasRoleData();
+            val metadata = new HasRoleMetadata();
+            stepStore.addMessage("The account has a role");
+            stepStore.setData(data);
+            stepStore.setMetadata(metadata);
+        }
+    }
+}
