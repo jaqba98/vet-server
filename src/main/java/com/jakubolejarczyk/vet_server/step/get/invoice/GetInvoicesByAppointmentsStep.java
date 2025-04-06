fa0ee359de1ac9c09332d3588b57f86ee840baae
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-06 04:20:34 +0200

    refactor: GetInvoicesByAppointmentsStep step.

---
+++ src/main/java/com/jakubolejarczyk/vet_server/step/get/invoice/GetInvoicesByAppointmentsStep.java
@@ -0,0 +1,26 @@
+package com.jakubolejarczyk.vet_server.step.get.invoice;
+
+import com.jakubolejarczyk.vet_server.model.dependent.Appointment;
+import com.jakubolejarczyk.vet_server.service.independent.InvoiceService;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GetInvoicesByAppointmentsStep<TData, TMetadata> implements StepRunnerModel<TData, TMetadata> {
+    private final InvoiceService invoiceService;
+
+    @Override
+    public void runStep(StepStore<TData, TMetadata> stepStore) {
+        if (stepStore.hasNotItem("appointmentsData")) throw new Error("The appointmentsData is required!");
+        val appointmentsData = stepStore.getItemAsArray("appointmentsData", Appointment.class);
+        val invoicesIds = appointmentsData.stream()
+            .map(Appointment::getInvoiceId)
+            .toList();
+        val invoicesData = invoiceService.findAllById(invoicesIds);
+        stepStore.setItem("invoicesData", invoicesData);
+    }
+}
