a81afca3eb726585968bf435ca474de6bbe2efb6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-07 21:12:59 +0200

    refactor: PetDeleteController

--- src/main/java/com/jakubolejarczyk/vet_server/controller/dependent/pet/PetDeleteController.java
+++ src/main/java/com/jakubolejarczyk/vet_server/controller/dependent/pet/PetDeleteController.java
@@ -1,67 +1,63 @@
-//package com.jakubolejarczyk.vet_server.controller.dependent.pet;
-//
-//import com.jakubolejarczyk.vet_server.dto.data.dependent.PetData;
-//import com.jakubolejarczyk.vet_server.dto.metadata.dependent.PetMetadata;
-//import com.jakubolejarczyk.vet_server.dto.request.logic.DeleteRequest;
-//import com.jakubolejarczyk.vet_server.dto.response.Response;
-//import com.jakubolejarczyk.vet_server.security.HandleValidationService;
-//import com.jakubolejarczyk.vet_server.step.check.CheckAccountPermissionToClientStep;
-//import com.jakubolejarczyk.vet_server.step.delete.DeletePetStep;
-//import com.jakubolejarczyk.vet_server.step.get.account.GetAccountByTokenStep;
-//import com.jakubolejarczyk.vet_server.step.get.GetClientByIdStep;
-//import com.jakubolejarczyk.vet_server.step.response.dependent.pet.PetDeleteResponseStep;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerController;
-//import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
-//import com.jakubolejarczyk.vet_server.store.StepStore;
-//import jakarta.validation.Valid;
-//import lombok.val;
-//import org.springframework.beans.factory.ObjectFactory;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.ArrayList;
-//
-//@RestController
-//@RequestMapping("/api/v1")
-//public class PetDeleteController extends StepRunnerController<PetData, PetMetadata> {
-//    private final GetAccountByTokenStep<PetData, PetMetadata> getAccountByTokenStep;
-//    private final GetClientByIdStep<PetData, PetMetadata> getClientByIdStep;
-//    private final CheckAccountPermissionToClientStep<PetData, PetMetadata> checkAccountPermissionToClientStep;
-//    private final DeletePetStep<PetData, PetMetadata> deletePetStep;
-//    private final PetDeleteResponseStep petDeleteResponseStep;
-//
-//    public PetDeleteController(
-//        ObjectFactory<StepStore<PetData, PetMetadata>> stepStoreObjectFactory,
-//        ObjectFactory<HandleValidationService> handleValidationServiceObjectFactory,
-//        GetAccountByTokenStep<PetData, PetMetadata> getAccountByTokenStep,
-//        GetClientByIdStep<PetData, PetMetadata> getClientByIdStep,
-//        CheckAccountPermissionToClientStep<PetData, PetMetadata> checkAccountPermissionToClientStep,
-//        DeletePetStep<PetData, PetMetadata> deletePetStep,
-//        PetDeleteResponseStep petDeleteResponseStep
-//    ) {
-//        super(stepStoreObjectFactory, handleValidationServiceObjectFactory);
-//        this.getAccountByTokenStep = getAccountByTokenStep;
-//        this.getClientByIdStep = getClientByIdStep;
-//        this.checkAccountPermissionToClientStep = checkAccountPermissionToClientStep;
-//        this.deletePetStep = deletePetStep;
-//        this.petDeleteResponseStep = petDeleteResponseStep;
-//    }
-//
-//    @PostMapping("pet-delete")
-//    public ResponseEntity<Response<PetData, PetMetadata>> petDelete(
-//        @Valid @RequestBody DeleteRequest request
-//    ) {
-//        val steps = new ArrayList<StepRunnerModel<PetData, PetMetadata>>();
-//        steps.addLast(getAccountByTokenStep);
-//        steps.addLast(getClientByIdStep);
-//        steps.addLast(checkAccountPermissionToClientStep);
-//        steps.addLast(deletePetStep);
-//        initController();
-//        getStepStore().setItem("token", request.getToken());
-//        getStepStore().setItem("petIds", request.getIds());
-//        return runController(steps, petDeleteResponseStep);
-//    }
-//}
+package com.jakubolejarczyk.vet_server.controller.dependent.pet;
+
+import com.jakubolejarczyk.vet_server.dto.data.dependent.PetData;
+import com.jakubolejarczyk.vet_server.dto.metadata.dependent.PetMetadata;
+import com.jakubolejarczyk.vet_server.dto.request.dependent.PetRequest;
+import com.jakubolejarczyk.vet_server.dto.request.logic.DeleteRequest;
+import com.jakubolejarczyk.vet_server.dto.response.Response;
+import com.jakubolejarczyk.vet_server.model.dependent.Pet;
+import com.jakubolejarczyk.vet_server.security.HandleValidationService;
+import com.jakubolejarczyk.vet_server.step.check.CheckAccountPermissionToClientStep;
+import com.jakubolejarczyk.vet_server.step.create.CreatePetStep;
+import com.jakubolejarczyk.vet_server.step.delete.DeletePetStep;
+import com.jakubolejarczyk.vet_server.step.get.account.GetAccountByTokenStep;
+import com.jakubolejarczyk.vet_server.step.get.client.GetClientByPetRequestStep;
+import com.jakubolejarczyk.vet_server.step.response.dependent.pet.PetCreateResponseStep;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerController;
+import com.jakubolejarczyk.vet_server.step_runner.StepRunnerModel;
+import com.jakubolejarczyk.vet_server.store.StepStore;
+import jakarta.validation.Valid;
+import lombok.val;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class PetDeleteController extends StepRunnerController<PetData, PetMetadata> {
+    private final GetAccountByTokenStep<PetData, PetMetadata> getAccountByTokenStep;
+    private final DeletePetStep<PetData, PetMetadata> deletePetStep;
+    private final PetCreateResponseStep petReadResponseStep;
+
+    public PetDeleteController(
+        ObjectFactory<StepStore<PetData, PetMetadata>> stepStoreObjectFactory,
+        ObjectFactory<HandleValidationService> handleValidationServiceObjectFactory,
+        GetAccountByTokenStep<PetData, PetMetadata> getAccountByTokenStep,
+        DeletePetStep<PetData, PetMetadata> deletePetStep,
+        PetCreateResponseStep petReadResponseStep
+    ) {
+        super(stepStoreObjectFactory, handleValidationServiceObjectFactory);
+        this.getAccountByTokenStep = getAccountByTokenStep;
+        this.deletePetStep = deletePetStep;
+        this.petReadResponseStep = petReadResponseStep;
+    }
+
+    @PostMapping("pet-delete")
+    public ResponseEntity<Response<PetData, PetMetadata>> petDelete(
+        @Valid @RequestBody DeleteRequest request
+    ) {
+        val steps = new ArrayList<StepRunnerModel<PetData, PetMetadata>>();
+        steps.addLast(getAccountByTokenStep);
+        steps.addLast(deletePetStep);
+        initController();
+        getStepStore().setItem("token", request.getToken());
+        getStepStore().setItem("petIds", request.getIds());
+        return runController(steps, petReadResponseStep);
+    }
+}
