bf7be32eb343c5fdc8d321fa6a0fe4c1d2b0a620
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-05 18:41:03 +0200

    initial commit

---
+++ src/test/java/com/olejarczykjakub/vet_server/VetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olejarczykjakub.vet_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
