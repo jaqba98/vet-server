fb3a3d4a79cfe06d7c11e14d6d27908ebd22b100
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-06 11:29:26 +0200

    add cors to the hello controller

--- src/main/java/com/olejarczykjakub/vet_server/HelloController.java
+++ src/main/java/com/olejarczykjakub/vet_server/HelloController.java
@@ -1,12 +1,16 @@
 package com.olejarczykjakub.vet_server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
     @GetMapping("/hello-world")
-    public String hello() {
-        return "Hello World!";
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello World!");
     }
 }
