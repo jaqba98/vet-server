5db2ced286a8e9baa57ef46d551a42f6990cbc68
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-05-06 17:41:39 +0200

    set up lombok and add getter and setters to the user model

--- src/main/java/com/olejarczykjakub/vet_server/model/UserModel.java
+++ src/main/java/com/olejarczykjakub/vet_server/model/UserModel.java
@@ -1,31 +1,12 @@
 package com.olejarczykjakub.vet_server.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserModel {
 	private String homeAccountId;
 	private String name;
 	private String username;
-
-	public String getHomeAccountId() {
-		return homeAccountId;
-	}
-
-	public void setHomeAccountId(String homeAccountId) {
-		this.homeAccountId = homeAccountId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 }
